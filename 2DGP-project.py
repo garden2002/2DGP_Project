bdd11ff87e179602e3be0402193d9826cfd3c61a
garden2002 <ehxhfl1017@naver.com>
2024-10-30 01:15:19 +0900

    state machine으로 구조 변경

--- 2DGP-project.py
+++ 2DGP-project.py
@@ -1,6 +1,31 @@
 from pico2d import *
 
+from state_machine import StateMachine, right_down, left_up, right_up, start_event, left_down
 
+
+class Idle:
+    @staticmethod
+    def enter(knight , e):
+        if right_down(e) or left_up(e):
+            pass
+        elif left_down(e) or right_up(e) or start_event(e):
+            pass
+        knight.frame = 0
+        knight.dir = 0
+        #시작 시간을 기록
+        knight.start_time = get_time()
+        pass
+    @staticmethod
+    def exit(knight , e):
+        pass
+    @staticmethod
+    def do(knight):
+        knight.frame = (knight.frame + 1) % 7
+        pass
+    @staticmethod
+    def draw(knight):
+        knight.image.clip_draw(knight.frame * 128, knight.action * 128, 128, 128, knight.x, knight.y)
+        pass
 
 class Knight:
     image = None
@@ -9,31 +34,25 @@ class Knight:
         self.frame = 0
         self.dir = 0
         self.action = 7
+        self.state_machine = StateMachine(self)  # 소년 객체를 위한 상태 머신인지 알려줄 필요
+        self.state_machine.start(Idle)  # 객체를 생성한게 아닌, 직접 Idle 이라는 클래스를 사용
+        self.state_machine.set_transitions(
+            {
+                Idle:{}
+            }
+        )
         if Knight.image == None:
             Knight.image = load_image('knight.png')
 
     def update(self):
-        self.frame = (self.frame + 1) % 7
-        self.x += self.dir * 5
+        self.state_machine.update()
 
     def handle_event(self, event):
-        if event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                self.dir += 1
-                self.action = 15
-            elif event.key == SDLK_LEFT:
-                self.dir -= 1
-                self.action = 15
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                self.dir -= 1
-                self.action = 7
-            elif event.key == SDLK_LEFT:
-                self.dir += 1
-                self.action = 7
-
+        self.state_machine.add_event(
+            ('INPUT', event)
+        )
     def draw(self):
-        self.image.clip_draw(self.frame * 128, self.action * 128, 128, 128, self.x, self.y)
+        self.state_machine.draw()
 
 def handle_events():
     global play
---
+++ state_machine.py
@@ -0,0 +1,64 @@
+# event (종류 문자열 , 실제 값)
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
+
+
+def start_event(e):
+    return e[0] == 'START'
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+# 상태 머신을 처리 관리해주는 클래스
+class StateMachine:
+    def __init__(self, o):
+        self.o = o #boy self가 전달, self.o 상테 머신과 연결된 캐릭터 객체
+        self.event_que = []
+        pass
+
+    def update(self):
+        self.cur_state.do(self.o)
+        #이벤트 발생했는지 확인하고, 거기에 따라서 상태변화
+        if self.event_que: # list에 요소가 있으면, list 값은 True
+            e = self.event_que.pop(0) #list에 첫번쨰 요소를 꺼냄
+            for check_event , next_state in self.transitions[self.cur_state].items():
+                if check_event(e):
+                    self.cur_state.exit(self.o , e)
+                    print(f'EXIT from {self.cur_state}')
+                    self.cur_state =next_state
+                    self.cur_state.enter(self.o , e)
+                    print(f'ENTER into {self.cur_state}')
+                    return
+        pass
+
+    def start(self, start_state):
+        #현재 상태를 시작 상태로 만듬
+        self.cur_state = start_state #Idle
+        self.cur_state.enter(self.o ,('START' , 0))
+        print(f'ENTER into {self.cur_state}')
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.o)
+        pass
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+        pass
+
+    def add_event(self, e):
+        self.event_que.append(e) # 상태머신용 이벤트 추가
+        print(f'    DEBUG: new event {e} is added.')
+        pass
